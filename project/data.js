80db69bcc03a07c2cf6a8447e628c9363f66df0a
侯炜 <houwei.dljgs1@bytedance.com>
2021-01-28 11:05:24 +0800

    first

---
+++ project/data.js
@@ -0,0 +1,487 @@
+var data_a1e2fb4a_e986_4524_b0da_9b7ba7c0874d = 
+{
+	"main": {
+		"floorIds": [
+			"sample0",
+			"sample1",
+			"sample2",
+			"sample3",
+			"MT0"
+		],
+		"floorPartitions": [],
+		"images": [
+			"bg.jpg",
+			"hero.png",
+			"winskin.png"
+		],
+		"tilesets": [
+			"magictower.png"
+		],
+		"animates": [
+			"hand",
+			"sword",
+			"zone"
+		],
+		"bgms": [
+			"bgm.mp3"
+		],
+		"sounds": [
+			"floor.mp3",
+			"attack.mp3",
+			"door.mp3",
+			"item.mp3",
+			"equip.mp3",
+			"zone.mp3",
+			"jump.mp3",
+			"pickaxe.mp3",
+			"bomb.mp3",
+			"centerFly.mp3"
+		],
+		"fonts": [],
+		"nameMap": {
+			"背景图.jpg": "bg.jpg",
+			"背景音乐.mp3": "bgm.mp3"
+		},
+		"levelChoose": [
+			{
+				"title": "简单",
+				"name": "Easy",
+				"hard": 1,
+				"action": [
+					{
+						"type": "comment",
+						"text": "在这里写该难度需执行的事件"
+					}
+				]
+			},
+			{
+				"title": "普通",
+				"name": "Normal",
+				"hard": 2,
+				"color": [
+					0,
+					255,
+					0,
+					1
+				],
+				"action": []
+			}
+		],
+		"equipName": [
+			"武器",
+			"盾牌"
+		],
+		"startBgm": null,
+		"styles": {
+			"startBackground": "project/images/bg.jpg",
+			"startVerticalBackground": "project/images/bg.jpg",
+			"startLogoStyle": "color: white",
+			"startButtonsStyle": "background-color: #32369F; opacity: 0.85; color: #FFFFFF; border: #FFFFFF 2px solid; caret-color: #FFD700;",
+			"statusLeftBackground": "url(project/materials/ground.png) repeat",
+			"statusTopBackground": "url(project/materials/ground.png) repeat",
+			"toolsBackground": "url(project/materials/ground.png) repeat",
+			"borderColor": [
+				204,
+				204,
+				204,
+				1
+			],
+			"statusBarColor": [
+				255,
+				255,
+				255,
+				1
+			],
+			"selectColor": [
+				255,
+				215,
+				0,
+				1
+			],
+			"floorChangingStyle": "background-color: black; color: white",
+			"font": "Verdana"
+		}
+	},
+	"firstData": {
+		"title": "魔塔样板",
+		"name": "template",
+		"version": "Ver 2.7.3",
+		"floorId": "sample0",
+		"hero": {
+			"image": "hero.png",
+			"animate": false,
+			"name": "阳光",
+			"lv": 1,
+			"hpmax": 9999,
+			"hp": 1000,
+			"manamax": -1,
+			"mana": 0,
+			"atk": 100,
+			"def": 100,
+			"mdef": 0,
+			"money": 0,
+			"exp": 0,
+			"equipment": [],
+			"items": {
+				"constants": {},
+				"tools": {},
+				"equips": {}
+			},
+			"loc": {
+				"direction": "up",
+				"x": 6,
+				"y": 10
+			},
+			"flags": {},
+			"followers": [],
+			"steps": 0
+		},
+		"startCanvas": [
+			{
+				"type": "comment",
+				"text": "在这里可以用事件来自定义绘制标题界面的背景图等"
+			},
+			{
+				"type": "comment",
+				"text": "也可以直接切换到其他楼层（比如某个开始剧情楼层）进行操作。"
+			},
+			{
+				"type": "showImage",
+				"code": 1,
+				"image": "bg.jpg",
+				"loc": [
+					0,
+					0
+				],
+				"opacity": 1,
+				"time": 0
+			},
+			{
+				"type": "while",
+				"condition": "1",
+				"data": [
+					{
+						"type": "comment",
+						"text": "给用户提供选择项，这里简单的使用了choices事件"
+					},
+					{
+						"type": "comment",
+						"text": "也可以贴按钮图然后使用等待操作来完成"
+					},
+					{
+						"type": "choices",
+						"choices": [
+							{
+								"text": "开始游戏",
+								"action": [
+									{
+										"type": "comment",
+										"text": "检查bgm状态，下同"
+									},
+									{
+										"type": "function",
+										"function": "function(){\ncore.control.checkBgm()\n}"
+									},
+									{
+										"type": "if",
+										"condition": "main.levelChoose.length == 0",
+										"true": [
+											{
+												"type": "comment",
+												"text": "直接开始游戏"
+											}
+										],
+										"false": [
+											{
+												"type": "comment",
+												"text": "动态生成难度选择项"
+											},
+											{
+												"type": "function",
+												"function": "function(){\nvar choices = [];\nmain.levelChoose.forEach(function (one) {\n\tchoices.push({\n\t\t\"text\": one.title || '',\n\t\t\"action\": [\n\t\t\t{ \"type\": \"function\", \"function\": \"function() { core.status.hard = '\" + (one.name || '') + \"'; }\" }\n\t\t]\n\t});\n})\ncore.insertAction({ \"type\": \"choices\", \"choices\": choices });\n}"
+											}
+										]
+									},
+									{
+										"type": "hideImage",
+										"code": 1,
+										"time": 0
+									},
+									{
+										"type": "comment",
+										"text": "成功选择难度"
+									},
+									{
+										"type": "break"
+									}
+								]
+							},
+							{
+								"text": "读取存档",
+								"action": [
+									{
+										"type": "function",
+										"function": "function(){\ncore.control.checkBgm()\n}"
+									},
+									{
+										"type": "comment",
+										"text": "简单的使用“呼出读档界面”来处理"
+									},
+									{
+										"type": "callLoad"
+									}
+								]
+							},
+							{
+								"text": "回放录像",
+								"action": [
+									{
+										"type": "function",
+										"function": "function(){\ncore.control.checkBgm()\n}"
+									},
+									{
+										"type": "comment",
+										"text": "这段代码会弹框选择录像文件"
+									},
+									{
+										"type": "if",
+										"condition": "(!core.isReplaying())",
+										"true": [
+											{
+												"type": "function",
+												"function": "function(){\ncore.chooseReplayFile()\n}"
+											}
+										]
+									}
+								]
+							}
+						]
+					}
+				]
+			},
+			{
+				"type": "comment",
+				"text": "接下来会执行startText中的事件"
+			}
+		],
+		"startText": [
+			{
+				"type": "comment",
+				"text": "初始剧情"
+			},
+			"Hi，欢迎来到 HTML5 魔塔样板！\n\n本样板由艾之葵制作，可以让你在不会写任何代码\n的情况下也能做出属于自己的H5魔塔！",
+			"这里游戏开始时的剧情。\n\n你可以在这里写上自己的内容。\n赶快来试一试吧！"
+		],
+		"shops": [
+			{
+				"id": "shop1",
+				"text": "\t[贪婪之神,moneyShop]勇敢的武士啊, 给我${20+2*flag:shop1}金币就可以：",
+				"textInList": "1F金币商店",
+				"mustEnable": false,
+				"disablePreview": false,
+				"choices": [
+					{
+						"text": "生命+800",
+						"need": "status:money>=20+2*flag:shop1",
+						"action": [
+							{
+								"type": "comment",
+								"text": "新版商店中需要手动扣减金币和增加访问次数"
+							},
+							{
+								"type": "setValue",
+								"name": "status:money",
+								"operator": "-=",
+								"value": "20+2*flag:shop1"
+							},
+							{
+								"type": "setValue",
+								"name": "flag:shop1",
+								"operator": "+=",
+								"value": "1"
+							},
+							{
+								"type": "setValue",
+								"name": "status:hp",
+								"operator": "+=",
+								"value": "800"
+							}
+						]
+					},
+					{
+						"text": "攻击+4",
+						"need": "status:money>=20+2*flag:shop1",
+						"action": [
+							{
+								"type": "comment",
+								"text": "新版商店中需要手动扣减金币和增加访问次数"
+							},
+							{
+								"type": "setValue",
+								"name": "status:money",
+								"operator": "-=",
+								"value": "20+2*flag:shop1"
+							},
+							{
+								"type": "setValue",
+								"name": "flag:shop1",
+								"operator": "+=",
+								"value": "1"
+							},
+							{
+								"type": "setValue",
+								"name": "status:atk",
+								"operator": "+=",
+								"value": "4"
+							}
+						]
+					}
+				]
+			},
+			{
+				"id": "shop2",
+				"text": "\t[贪婪之神,expShop]勇敢的武士啊, 给我一定经验就可以：",
+				"textInList": "1F经验商店",
+				"mustEnable": false,
+				"disablePreview": true,
+				"choices": [
+					{
+						"text": "等级+1（100经验）",
+						"need": "status:exp>=100",
+						"action": [
+							{
+								"type": "setValue",
+								"name": "status:exp",
+								"operator": "-=",
+								"value": "100"
+							},
+							{
+								"type": "setValue",
+								"name": "status:lv",
+								"operator": "+=",
+								"value": "1"
+							},
+							{
+								"type": "setValue",
+								"name": "status:hp",
+								"operator": "+=",
+								"value": "1000"
+							}
+						]
+					}
+				]
+			},
+			{
+				"id": "itemShop",
+				"item": true,
+				"textInList": "道具商店",
+				"mustEnable": false,
+				"choices": [
+					{
+						"id": "yellowKey",
+						"number": 10,
+						"money": "10",
+						"sell": "5"
+					}
+				]
+			},
+			{
+				"id": "keyShop",
+				"textInList": "回收钥匙商店",
+				"mustEnable": false,
+				"commonEvent": "回收钥匙商店"
+			}
+		],
+		"levelUp": [
+			{
+				"need": "0",
+				"title": "",
+				"action": [
+					{
+						"type": "comment",
+						"text": "此处是初始等级，只需填写称号"
+					}
+				]
+			},
+			{
+				"need": "20",
+				"title": "",
+				"action": [
+					{
+						"type": "setValue",
+						"name": "status:atk",
+						"operator": "+=",
+						"value": "10"
+					},
+					{
+						"type": "setValue",
+						"name": "status:def",
+						"operator": "+=",
+						"value": "10"
+					}
+				]
+			},
+			{
+				"need": "40",
+				"title": "",
+				"action": [
+					{
+						"type": "tip",
+						"text": "恭喜升级"
+					}
+				]
+			}
+		]
+	},
+	"values": {
+		"lavaDamage": 100,
+		"poisonDamage": 10,
+		"weakValue": 20,
+		"redGem": 3,
+		"blueGem": 3,
+		"greenGem": 5,
+		"redPotion": 100,
+		"bluePotion": 250,
+		"yellowPotion": 500,
+		"greenPotion": 800,
+		"breakArmor": 0.9,
+		"counterAttack": 0.1,
+		"purify": 3,
+		"hatred": 2,
+		"animateSpeed": 400,
+		"statusCanvasRowsOnMobile": 3,
+		"floorChangeTime": 500
+	},
+	"flags": {
+		"statusBarItems": [
+			"enableFloor",
+			"enableHP",
+			"enableAtk",
+			"enableDef",
+			"enableMDef",
+			"enableMoney",
+			"enableKeys"
+		],
+		"flyNearStair": true,
+		"flyRecordPosition": false,
+		"steelDoorWithoutKey": false,
+		"itemFirstText": false,
+		"equipboxButton": false,
+		"enableAddPoint": false,
+		"enableNegativeDamage": false,
+		"betweenAttackMax": false,
+		"useLoop": false,
+		"startUsingCanvas": false,
+		"statusCanvas": false,
+		"displayEnemyDamage": true,
+		"displayCritical": true,
+		"displayExtraDamage": true,
+		"enableGentleClick": true,
+		"ignoreChangeFloor": true,
+		"canGoDeadZone": false,
+		"enableMoveDirectly": true,
+		"enableRouteFolding": true,
+		"disableShopOnDamage": false,
+		"blurFg": false
+	}
+}
